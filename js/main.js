84a28d205bed9aa6737ebc30e9b0079515133de9
cookjohnhay <johnhay@cookbook.ph>
2021-09-12 03:11:25 +0800

    fixed panolens error

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const panoImage = document.querySelector('.pano');
 const samplePanoPath = 'https://unsplash.com/photos/8UDJ4sflous';
 
+const panorama = new PANOLENS
+
 const panorama = new PANOLENS.ImagePanorama(samplePanoPath);
 const viewer = new PANOLENS.Viewer({
     container: panoImage
