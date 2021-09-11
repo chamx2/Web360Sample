3a78bd849f0ce8eb801826c1c274cd784c9fc9c0
cookjohnhay <johnhay@cookbook.ph>
2021-09-12 02:34:56 +0800

    fix main js bug

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 const panoImage = document.querySelector('.pano-image');
-const samplePano = 'images/pano1.jpg';
+const samplePanoPath = '../images/pano1.jpg';
 
-const panorama = new PANOLENS.ImagePanorama(samplePano);
-const viewer = new PANOLENS.viewer({
+const panorama = new PANOLENS.ImagePanorama(samplePanoPath);
+const viewer = new PANOLENS.Viewer({
     container: panoImage
 });
 
