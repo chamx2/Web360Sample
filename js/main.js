0aae942cec74de551b99ea3a993d7c118f8c63ad
cookjohnhay <johnhay@cookbook.ph>
2021-09-12 04:09:07 +0800

    reversed main.js script to original

--- js/main.js
+++ js/main.js
@@ -2,13 +2,12 @@ const panoImage = document.querySelector('.pano');
 const samplePanoPath = 'images/pano3.jpg';
 
 
-// const panorama = new PANOLENS.ImagePanorama(samplePanoPath);
-const panorama = new PANOLENS.ImagePanorama('images/pano3.jpg');
+const panorama = new PANOLENS.ImagePanorama(samplePanoPath);
+// const panorama = new PANOLENS.ImagePanorama('images/pano3.jpg');
 const viewer = new PANOLENS.Viewer({
     container:panoImage,
     autoRotate:true,
     autoRotateSpeed: .5
-
 });
 
 viewer.add(panorama);
