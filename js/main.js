50affe1274d97c5891b90ee16fd800ffe088e90d
cookjohnhay <johnhay@cookbook.ph>
2021-09-12 16:32:07 +0800

    added package.json for dependencies list

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ const panoImage = document.querySelector('.pano-container');
 const samplePanoPath = 'images/pano3.jpg';
 
 
+// const panorama = new PANOLENS.ImagePanorama(samplePanoPath);
+
+// const panorama =  PANOLENS.ImagePanorama('../images/pano3.jpg');
+// const viewer = PANOLENS.Viewer();
+
+// const viewer = new PANOLENS.Viewer({
+//     container:panoImage,
+//     autoRotate:true,
+//     autoRotateSpeed: .5
+// });
 const panorama = new PANOLENS.ImagePanorama(samplePanoPath);
-// const panorama = new PANOLENS.ImagePanorama('images/pano3.jpg');
-const viewer = new PANOLENS.Viewer({
-    container:panoImage,
-    autoRotate:true,
-    autoRotateSpeed: .5
-});
+const viewer = new PANOLENS.Viewer();
 
 viewer.add(panorama);
